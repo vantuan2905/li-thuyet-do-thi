84c7723723a7a087e1bb2ed4d7bf82cabe677405
tuấn naive <99082087+vantuan2905@users.noreply.github.com>
2022-06-28 23:11:12 +0700

    Add files via upload

---
+++ chuyen ma tran ke sang danh sach ke.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+main(){
+	int t;
+	t=1;
+	while(t--){
+		int a[200][200];
+		int n;
+		cin>>n;
+		for(int i=1;i<=n;i++){
+		 for(int j=1;j<=n;j++){
+		  cin>>a[i][j];
+		  if(a[i][j]==1) cout<<j<<" ";
+}cout<<endl;}
+	}
+}
