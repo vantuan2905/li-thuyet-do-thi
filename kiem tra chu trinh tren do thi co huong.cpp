07ef0c1f2dc938fa5500ed46eb4933aa71a90117
tuấn naive <99082087+vantuan2905@users.noreply.github.com>
2022-07-01 23:06:11 +0700

    Add files via upload

---
+++ kiem tra chu trinh tren do thi co huong.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int n,m,ok[1005];vector<int> v[1005];int kt,c;
+void dfs(int i,int cnt){
+	ok[i]=1;
+	if(kt==1) return ;
+	if(cnt>1){
+	    for(int j=0;j<v[i].size();j++){
+			if(v[i][j]==c){
+			kt=1;return;
+			}
+	 	}
+	}
+	for(int j=0;j<v[i].size();j++) if(ok[v[i][j]]==0) dfs(v[i][j],cnt+1);
+}
+main(){
+	int t;
+	cin>>t;
+	while(t--){
+		memset(v,0,sizeof(v));
+		cin>>n>>m;
+		for(int i=0;i<m;i++){
+			int a,b;
+			cin>>a>>b;
+			v[a].push_back(b);
+			//v[b].push_back(a);
+		}kt=0;
+		for(int i=1;i<=n&&kt==0;i++){
+			memset(ok,0,sizeof(ok));c=i;
+			dfs(i,0);
+		}
+		if(kt) cout<<"YES\n";
+		else cout<<"NO\n"; 
+	}
+}
